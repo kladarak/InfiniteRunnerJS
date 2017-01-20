5937ebdeb90424a79587e937d4cf77d0e75841b4
SUPERMASSIVE\scott.jenkins <scottjenkins88@gmail.com>
2017-01-20 15:45:07 +0000

    Made scrolling speed gradually increase.

--- platformUpdater.js
+++ platformUpdater.js
@@ -2,9 +2,12 @@ function PlatformUpdater()
 {
 	this.update = function(world)
 	{
+		var speed = world.player.score / 2000;
+		speed += 2;
+		
 		world.platforms.forEach(function (p)
 		{
-			p.x -= 2;
+			p.x -= speed;
 			p.isOnScreen = (p.x + p.width) > 0;
 		});
 		
